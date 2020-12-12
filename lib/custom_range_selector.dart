d11a1a1854ecab422ab6ca773f93c41f58bd26bb
Emrullah <66071467+e-demir@users.noreply.github.com>
2020-12-12 21:43:14 +0300

    init

---
+++ lib/custom_range_selector.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomRangeSelector extends StatefulWidget {
+  @override
+  _CustomRangeSelectorState createState() => _CustomRangeSelectorState();
+}
+
+class _CustomRangeSelectorState extends State<CustomRangeSelector> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
