93023d3b2865cb0a27219db517a55b2796a80584
Emrullah <66071467+e-demir@users.noreply.github.com>
2020-12-12 21:56:18 +0300

    CustomRangeSelector added to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_range_selector/custom_range_selector.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,17 +19,43 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double startPoint = 0.25;
+
+  double endPoint = 0.75;
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     double rangeSelectorWidth = size.width * 0.75;
     double rangeSelectorHeight = rangeSelectorWidth * 0.25;
     return Scaffold(
+      backgroundColor: Colors.pinkAccent,
       body: Center(
-
+        child: CustomRangeSelector(
+          width: rangeSelectorWidth,
+          heignt: rangeSelectorHeight,
+          divisions: 10,
+          start: startPoint,
+          end: endPoint,
+          onStartChange: (value){
+            setState(() {
+              startPoint=value;
+            });
+          },
+          onEndChange: (value){
+            setState(() {
+              endPoint=value;
+            });
+          },
+        ),
       ),
     );
   }
 }
-
